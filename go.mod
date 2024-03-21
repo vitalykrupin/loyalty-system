b56a3a71ab869246918b4744a5e2203f13f13d47
Vitaly Krupin <vitalykrupin@gmail.com>
2024-03-21 18:25:59 +0500

    Add config

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vitalykrupin/loyalty-system
 go 1.21.3
 
 require (
+	github.com/caarlos0/env/v10 v10.0.0
 	github.com/go-chi/chi/v5 v5.0.12
 	go.uber.org/zap v1.27.0
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"flag"
+	"github.com/caarlos0/env/v10"
+	"log"
+)
+
+type Config struct {
+	ServerAddress        string `env:"RUN_ADDRESS"`
+	DBDSN                string `env:"DATABASE_DSN"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
+	MigrationDir         string
+}
+
+const (
+	defaultServerAddress = "localhost:8080"
+	defaultDBDSN         = "postgres://postgres:pwd@localhost:5432/postgres?sslmode=disable"
+	defaultAccrualSystem = "localhost:8181"
+	migrationDir         = "./internal/storage/migrations"
+)
+
+func NewConfig() *Config {
+	return &Config{
+		ServerAddress:        defaultServerAddress,
+		DBDSN:                defaultDBDSN,
+		AccrualSystemAddress: defaultAccrualSystem,
+		MigrationDir:         migrationDir,
+	}
+}
+
+func (c *Config) ParseFlags() error {
+	flag.Func("a", "example: '-a localhost:8080'", func(addr string) error {
+		c.ServerAddress = addr
+		return nil
+	})
+	flag.Func("d", "example: '-d postgres://postgres:pwd@localhost:5432/postgres?sslmode=disable'", func(dsn string) error {
+		c.DBDSN = dsn
+		return nil
+	})
+	flag.Func("r", "example: '-r localhost:8181'", func(addr string) error {
+		c.AccrualSystemAddress = addr
+		return nil
+	})
+	flag.Parse()
+
+	err := env.Parse(c)
+	if err != nil {
+		log.Println("Config is not available", err)
+		return err
+	}
+	return nil
+}
